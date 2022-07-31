e11cccbb5f4850423c34b05b767a79018c08be49
oleg <oleg@muraveyko.ru>
2022-07-31 11:22:42 +0300

    Перехожу к попытке разверстать экран аналогично уже опубликованному приложению

--- lib/view_screen.dart
+++ lib/view_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:npd/dao/database.dart';
-import 'generated/l10n.dart';
 import 'model/receipt.dart';
 
 
@@ -17,23 +16,47 @@ class ViewScreenState extends State<ViewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(S.of(context).menu_settings),
-        ),
-        body: StreamBuilder(
+    return StreamBuilder(
           stream:  NpdDao.modelReceipt?.getReceipt(widget.receiptId, widget.inn),
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
             if (snapshot.hasError) {
-              return Text("Error ${snapshot.error}");
+              return Scaffold(
+                  appBar: AppBar(
+                    title: Text(widget.receiptId),
+                  ),
+                  body:Text("Error ${snapshot.error}")
+              );
             }
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
+              return Scaffold(
+                  appBar: AppBar(
+                    title: Text(widget.receiptId),
+                  ),
+                  body:const CircularProgressIndicator()
+              );
             }
             Receipt receipt = snapshot.data;
-            return Text(receipt.receiptId!);
-          },),
-
+            return Scaffold(
+                body: CustomScrollView(
+                slivers: <Widget>[
+                    SliverAppBar(
+                      expandedHeight: 160.0,
+                      flexibleSpace: FlexibleSpaceBar(
+                          title: Text(receipt.receiptId!),
+                      ),
+                    ),
+                    const SliverToBoxAdapter(
+                      child: SizedBox(
+                      height: 20,
+                      child: Center(
+                          child: Text('Scroll to see the SliverAppBar in effect.'),
+                        ),
+                      ),
+                    ),
+                ]
+                )
+            );
+          },
 
     );
   }
